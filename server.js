a331236608689e1670ee130263c67674cf4497ac
Joe Brimeyer <jbrimeyer@gmail.com>
2021-05-04 12:56:36 -0700

    updated file structure, added functional server, created package.json.

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// Middleware
+const methodOverride = require("method-override");
+const session = require("express-session");
+
+app.use(express.json()); // Used to parse JSON bodies.
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies.
+
+// Serve up static assets (usually on heroku).
+app.use(express.static("client/build"));
+
+// Routes.
+const userRouter = require("./routes/users.js");
+app.use("/users", userRouter);
+
+// Server connection.
+app.listen(PORT, () => {
+  console.log(`🌎 ==> Server is now listening on port ${PORT}!`);
+});
