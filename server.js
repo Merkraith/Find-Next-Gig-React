dfca15f9a521d7a8a1466b3131ff647227b0c538
Joe Brimeyer <jbrimeyer@gmail.com>
2021-05-04 13:09:06 -0700

    removed node_modules.

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
 const path = require("path");
 
 const PORT = process.env.PORT || 3000;
@@ -9,16 +8,11 @@ const app = express();
 // Middleware
 const methodOverride = require("method-override");
 const session = require("express-session");
-
 app.use(express.json()); // Used to parse JSON bodies.
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies.
 
 // Serve up static assets (usually on heroku).
 app.use(express.static("client/build"));
-
-// Routes.
-const userRouter = require("./routes/users.js");
-app.use("/users", userRouter);
 
 // Server connection.
 app.listen(PORT, () => {
