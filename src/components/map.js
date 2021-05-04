44b48f330f047bc06d791db05a5fb81f7e9bee08
Mark Heil <mark.heil1122@gmail.com>
2021-05-03 22:34:41 -0700

    base code for map display card

---
+++ src/components/map.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Card from 'react-bootstrap/Card';
+
+
+function Map() {
+    return (
+        <div>
+            <Card style={{ width: '36rem' }}>
+                <Card.Body>
+                    Map Goes Here
+                </Card.Body>
+            </Card>
+        </div>
+    );
+}
+
+export default Map;
